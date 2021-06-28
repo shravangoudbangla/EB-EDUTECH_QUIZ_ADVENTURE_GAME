b69605bc703967e99e7e083dee35ec8dbcb83fd0
BANGLA SHRAVAN KUMAR GOUD <79461931+shravangoudbangla@users.noreply.github.com>
2021-06-28 19:20:26 +0530

    Add files via upload

---
+++ Animals.js
@@ -0,0 +1,119 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+   var x = document.getElementById("image").src;
+  
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+
+
+const questions = [
+  {
+      question: 'What animals went into space?',
+      answers: [
+      { text: 'cat', correct: false },
+      { text: 'Dog', correct: true }
+     
+    ]
+  },
+  {
+    question: 'Which animal take breathes through skin?',
+    answers: [
+      { text: 'frog', correct: true },
+      { text: 'cat', correct: false },
+      { text: 'snake', correct: false },
+      { text: 'Fish', correct: false }
+    ]
+  },
+  {
+    question: 'The tallest living animal is the?',
+    answers: [
+      { text: 'Elephants', correct: false },
+      { text: 'Giraffe', correct: true },
+      { text: 'Cheetah', correct: false },
+      { text: 'Lion', correct: false }
+    ]
+  },
+  {
+    question: 'A camel can remain without water for?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '30', correct: true }
+    ]
+  }
+]
+
+ 
